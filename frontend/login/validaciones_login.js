2530740c2be7692cb463b8a4845569bf9b054bdf
agustinlehmann <agustinlehmann20@gmail.com>
2024-08-26 18:13:22 -0300

    Resolución de conflictos 2

--- frontend/login/validaciones_login.js
+++ frontend/login/validaciones_login.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('form');
-  
+
   form.addEventListener('submit', function(event) {
       const email = document.getElementById('email').value.trim();
       const password = document.getElementById('password').value.trim();
       let errorMessage = '';
       const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
       if (!emailPattern.test(email)) {
           errorMessage += 'Por favor, ingresa un correo electrónico válido.\n';
       }
@@ -25,29 +26,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function iniciarSesion(email, password) {
   if (email === 'admin10@gmail.com' && password === '12345') {
-    window.location.href = '../indexadmin.html';
-  } else {
-    const cuenta = {
-      email: email,
-      password: password
-    };
-    fetch('http://localhost:3000/api/usuarios/iniciar-sesion', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', 
-      },
-      body: JSON.stringify(cuenta)
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+      localStorage.setItem('userRole', 'admin');
       window.location.href = '../index.html';
-      return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        localStorage.setItem('authToken', data.token);
-    });
+  } else {
+      const cuenta = {
+          email: email,
+          password: password
+      };
+      fetch('http://localhost:3000/api/usuarios/iniciar-sesion', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(cuenta)
+      })
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+      })
+      .then(data => {
+          console.log(data);
+          localStorage.setItem('authToken', data.token);
+          localStorage.setItem('userRole', 'user'); // Usuario normal
+          window.location.href = '../index.html';
+      });
   }
 }
